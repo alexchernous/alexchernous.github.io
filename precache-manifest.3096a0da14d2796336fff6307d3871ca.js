b9f09a15c342ee8c0d8ca5f27e58bb63448310a7
alex chernous <alex.a.chernous@gmail.com>
2019-12-05 22:58:27 -0500

    Updates

---
+++ precache-manifest.3096a0da14d2796336fff6307d3871ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c29d055131d6968f1aa4dc0fd22ad65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66a9f3726ae718d1598f",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d92876378b28c7c87ad4",
+    "url": "/static/css/main.8247c2e1.chunk.css"
+  },
+  {
+    "revision": "66a9f3726ae718d1598f",
+    "url": "/static/js/2.903fb4eb.chunk.js"
+  },
+  {
+    "revision": "d92876378b28c7c87ad4",
+    "url": "/static/js/main.94d6839d.chunk.js"
+  },
+  {
+    "revision": "216030cd1ab33a8a8bbc",
+    "url": "/static/js/runtime-main.3546c973.js"
+  },
+  {
+    "revision": "cb086cd5f3d835df7baad0cbd6f7b530",
+    "url": "/static/media/AboutMe.cb086cd5.md"
+  },
+  {
+    "revision": "4ce40ec326b297f49e9387bf0f107378",
+    "url": "/static/media/AlexChernousResume.4ce40ec3.pdf"
+  },
+  {
+    "revision": "2c75831a65930daa4861ea1d32dab82d",
+    "url": "/static/media/Home.2c75831a.md"
+  }
+]);
